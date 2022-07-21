0d8126b0775b87327af1df8f94b597c958a9ed52
hlongng2002241 <nguyenhailongng@gmail.com>
2022-07-21 23:11:49 +0700

    update

---
+++ best_xgB_scale_basetunning.py
@@ -0,0 +1,177 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from imblearn.pipeline import Pipeline
+
+from sklearn.preprocessing import MinMaxScaler
+
+from imblearn.over_sampling import SMOTE, BorderlineSMOTE, SVMSMOTE, ADASYN
+
+from sklearn.linear_model import LogisticRegression, Perceptron
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+# from lightgbm import LGBMClassifier
+from xgboost import XGBClassifier
+
+from sklearn.experimental import enable_halving_search_cv
+from sklearn.model_selection import GridSearchCV, HalvingGridSearchCV, train_test_split, RepeatedStratifiedKFold, cross_val_score, learning_curve
+from sklearn.metrics import classification_report, fbeta_score, confusion_matrix, precision_recall_curve, auc, make_scorer
+
+accepted_loans = pd.read_csv('./data/elite.csv')
+
+X = accepted_loans.loc[:, accepted_loans.columns != 'loan_paid'].values
+y = accepted_loans['loan_paid'].values
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y)
+
+cv = RepeatedStratifiedKFold(n_splits=5, n_repeats=2, random_state=42)
+
+minmax_scaler = ('minmax', MinMaxScaler())
+
+smote = ('smote', SMOTE(sampling_strategy=1.0, random_state=42))
+smote_param_grid = {
+    'smote__sampling_strategy': [0.8, 0.9, 1.0]
+    # 'smote__k_neighbors': [3, 5, 10, 15]
+}
+
+# mlp = ('mlp', MLPClassifier(max_iter=100, random_state=42,
+#        early_stopping=True, activation='logistic', n_iter_no_change=20))
+# mlp_param_grid = {
+#     'mlp__hidden_layer_sizes': [(100,), (100, 100), (50, 50, 50), (50, 100, 50)],
+# }
+
+f_onehalf_scorer = make_scorer(fbeta_score, beta=0.5)
+
+
+def plot_learning_curve(
+    estimator,
+    title,
+    X,
+    y,
+    axes=None,
+    ylim=None,
+    cv=cv,
+    n_jobs=-1,
+    train_sizes=np.linspace(0.1, 1.0, 5),
+):
+    if axes is None:
+        _, axes = plt.subplots(3, 1, figsize=(15, 15))
+
+    axes[0].set_title(title)
+    if ylim is not None:
+        axes[0].set_ylim(*ylim)
+    axes[0].set_xlabel("Training examples")
+    axes[0].set_ylabel("Score")
+
+    train_sizes, train_scores, test_scores, fit_times, _ = learning_curve(
+        estimator,
+        X,
+        y,
+        cv=cv,
+        n_jobs=n_jobs,
+        train_sizes=train_sizes,
+        scoring=f_onehalf_scorer,
+        return_times=True,
+        random_state=42
+    )
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    fit_times_mean = np.mean(fit_times, axis=1)
+    fit_times_std = np.std(fit_times, axis=1)
+
+    # Plot learning curve
+    axes[0].grid()
+    axes[0].fill_between(
+        train_sizes,
+        train_scores_mean - train_scores_std,
+        train_scores_mean + train_scores_std,
+        alpha=0.1,
+        color="r",
+    )
+    axes[0].fill_between(
+        train_sizes,
+        test_scores_mean - test_scores_std,
+        test_scores_mean + test_scores_std,
+        alpha=0.1,
+        color="g",
+    )
+    axes[0].plot(
+        train_sizes, train_scores_mean, "o-", color="r", label="Training score"
+    )
+    axes[0].plot(
+        train_sizes, test_scores_mean, "o-", color="g", label="Cross-validation score"
+    )
+    axes[0].legend(loc="best")
+
+    # Plot n_samples vs fit_times
+    axes[1].grid()
+    axes[1].plot(train_sizes, fit_times_mean, "o-")
+    axes[1].fill_between(
+        train_sizes,
+        fit_times_mean - fit_times_std,
+        fit_times_mean + fit_times_std,
+        alpha=0.1,
+    )
+    axes[1].set_xlabel("Training examples")
+    axes[1].set_ylabel("fit_times")
+    axes[1].set_title("Scalability of the model")
+
+    # Plot fit_time vs score
+    fit_time_argsort = fit_times_mean.argsort()
+    fit_time_sorted = fit_times_mean[fit_time_argsort]
+    test_scores_mean_sorted = test_scores_mean[fit_time_argsort]
+    test_scores_std_sorted = test_scores_std[fit_time_argsort]
+    axes[2].grid()
+    axes[2].plot(fit_time_sorted, test_scores_mean_sorted, "o-")
+    axes[2].fill_between(
+        fit_time_sorted,
+        test_scores_mean_sorted - test_scores_std_sorted,
+        test_scores_mean_sorted + test_scores_std_sorted,
+        alpha=0.1,
+    )
+    axes[2].set_xlabel("fit_times")
+    axes[2].set_ylabel("Score")
+    axes[2].set_title("Performance of the model")
+
+    return plt
+
+
+# param_grid = {**mlp_param_grid}
+
+#no tuning required, default smote is best model
+pipe = Pipeline(steps=[('minmax', MinMaxScaler()),
+                       ('xgBoost',XGBClassifier(n_estimators = 500,
+                                                max_depth = 6,
+                                                reg_alpha = 1.0,
+                                                reg_lambda = 1.0)
+                      )]
+                )
+
+pipe.fit(X_train, y_train)
+
+y_pred = pipe.predict(X_test)
+
+f_onehalf_score = fbeta_score(y_test, y_pred, beta=0.5)
+print('f0.5_score=', f_onehalf_score)
+
+name = "MinMaxScaler + Tunning xgBoost"
+
+try:
+    y_score = pipe.predict_proba(X_test)[:, 1]
+    # calculate precision and recall for each threshold
+    precision, recall, threshold = precision_recall_curve(y_test, y_score)
+    # calculate scores
+    pr_auc = auc(recall, precision)
+    print('pr_auc_score=', pr_auc)
+except:
+    pass
+
+plot_learning_curve(pipe, name, X_train, y_train, cv=cv, n_jobs=-1)
+plt.savefig("best_xgB_scale_basetunning.png")
